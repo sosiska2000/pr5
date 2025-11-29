c282347823a34bd3cb96fd43c68ec408b711bf37
sosiska2000 <maratoleg2006@gmail.com>
2025-11-30 00:52:22 +0500

    запрос для получения уникального кода

---
+++ Server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void ConnectServer()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            IPEndPoint endPoint = new IPEndPoint(ServerIpAddress, ServerPort);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(endPoint);
+
+            string msg = $"/connect {login} {password}";
+            socket.Send(Encoding.UTF8.GetBytes(msg));
+
+            byte[] buffer = new byte[1024];
+            int size = socket.Receive(buffer);
+            string response = Encoding.UTF8.GetString(buffer, 0, size);
+
+            if (response == "/auth_fail")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Неверный логин или пароль.");
+                return;
+            }
+
+            if (response == "/banned")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ваш аккаунт находится в черном списке!");
+                return;
+            }
+
+            if (response == "/limit")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Нет свободных лицензий.");
+                return;
+            }
+
+            ClientToken = response;
+            ClientDateConnection = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Успешное подключение. Ваш токен: " + ClientToken);
+        }
+    }
+}
